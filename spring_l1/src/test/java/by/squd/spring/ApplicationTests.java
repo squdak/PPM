a0032d6185cb1a9df296be363e12177dead432b2
AK <squd@tut.by>
2020-10-23 22:41:39 +0300

    first commit

---
+++ spring_l1/src/test/java/by/squd/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.squd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
